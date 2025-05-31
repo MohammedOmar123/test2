ef0c0432fb35b9b354dd0107dfb2bc311faacc6c
Mohammed Omar Balousha <mbalousha@bayt.net>
2025-05-31 06:27:51 +0300

    some changes on tetsFolder files , and add server.js

---
+++ tetsFolder/index.js
@@ -0,0 +1,16 @@
+console.log('This is the index.js file in the tetsFolder directory.');
+// This file is used to demonstrate the structure of a directory with an index.js file.
+// It can be used to export modules or perform initializations when the directory is imported.
+// You can add more functionality here as needed.
+// For example, you can export functions or variables from this file:
+export function greet() {
+    return 'Hello from the tetsFolder!';
+}
+// You can also import this function in other files like this:
+// import { greet } from './tetsFolder/index.js';
+// Note: Ensure that the file is named 'index.js' to be recognized as the entry point of the directory.
+// This file is part of a larger project structure and can be used to organize code effectively.
+// You can also add comments to explain the purpose of this file or any specific logic implemented here.
+// This file is part of a larger project structure and can be used to organize code effectively.
+
+log('This is the index.js file in the tetsFolder directory.');
